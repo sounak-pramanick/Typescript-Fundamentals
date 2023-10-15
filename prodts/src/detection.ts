055666323f55e0add36e92f310daf9d5a3c79461
Sounak Pramanick <sounakmsd7@gmail.com>
2023-10-15 20:40:58 +0530

    Discriminated unions

--- prodts/src/detection.ts
+++ prodts/src/detection.ts
@@ -83,3 +83,30 @@ function getFood(pet: Fish | Bird) {
     }
 }
 
+// discriminated unions
+interface Circle {
+    kind: "circle",
+    radius: number
+}
+
+interface Square {
+    kind: "square",
+    side: number
+}
+
+interface Rectangle {
+    kind: "rectangle",
+    length: number,
+    width: number
+}
+
+type Shape = Circle | Square | Rectangle;
+
+function getTrueShape(shape: Shape) {
+    if(shape.kind === "circle") {
+        return Math.PI * shape.radius ** 2;
+    }
+    
+    // return shape.side * shape.side;
+}
+
